3dfd47f7caf7c23aa193ce9812bdcae065069ed8
Maruf Robin <maruf.robin@northsouth.edu>
2022-12-24 00:21:04 +0600

    modal bottom sheet added and image added.

--- lib/screen/feed_page.dart
+++ lib/screen/feed_page.dart
@@ -1,3 +1,4 @@
+import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:datting_app/model/model_data.dart';
 import 'package:flutter/material.dart';
 
@@ -47,30 +48,68 @@ class FeedPage extends StatelessWidget {
           scrollDirection: Axis.vertical,
           itemCount: modelData.length,
           itemBuilder: (context, index) {
-            return Container(
-              margin: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 4,
-                        blurRadius: 8,
-                        offset: Offset(0, 3))
-                  ],
-                  image: DecorationImage(
-                      image: AssetImage(
-                        "${modelData[index].img}",
-                      ),
-                      fit: BoxFit.cover)
-                  // color: Colors.cyanAccent,
-                  ),
+            return GestureDetector(
+              onTap: () {
+                buildProfileBottomSheet(context, index);
+              },
+              child: Container(
+                margin: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 4,
+                          blurRadius: 8,
+                          offset: Offset(0, 3))
+                    ],
+                    image: DecorationImage(
+                        image: AssetImage(
+                          "${modelData[index].img}",
+                        ),
+                        fit: BoxFit.cover)
+                    // color: Colors.cyanAccent,
+                    ),
+              ),
             );
           },
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, mainAxisExtent: 260),
         ),
       ),
+    );
+  }
+
+  Future<dynamic> buildProfileBottomSheet(BuildContext context, int index) {
+    return showModalBottomSheet(
+      barrierColor: Colors.transparent,
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (context) {
+        return Container(
+          margin: EdgeInsets.all(16),
+          child: BlurryContainer(
+            blur: 10, color: Colors.white.withOpacity(0.5),
+            height: 430,
+            // elevation: 6,
+            child: Column(
+              children: [
+                Container(
+                  height: 300,
+                  width: 280,
+                  // margin: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          image: AssetImage(modelData[index].img),
+                          fit: BoxFit.cover)),
+                ),
+                Expanded(child: Placeholder())
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
